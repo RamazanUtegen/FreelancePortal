b09ee2bba318c53b2b3f8d3e5dc7ba4d947b6bad
ramaz <254128@astanait.edu.kz>
2026-01-08 12:00:00 +0500

    Added Main class to test the system

--- src/freelancee/Main.java
+++ src/freelancee/Main.java
@@ -1,13 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+package freelancee;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        Portal portal = new Portal("FreelanceHub");
+
+        Freelancer f1 = new Freelancer("Alex", "Java");
+        Freelancer f2 = new Freelancer("Maria", "Java");
+        Freelancer f3 = new Freelancer("John", "Python");
+
+        JobListing j1 = new JobListing("Website Development", 5000);
+        JobListing j2 = new JobListing("Mobile App", 700);
+
+        portal.addFreelancer(f1);
+        portal.addFreelancer(f2);
+        portal.addFreelancer(f3);
+
+        portal.addJob(j1);
+        portal.addJob(j2);
+
+        System.out.println(portal);
+        System.out.println(f1);
+        System.out.println(j1);
+
+        System.out.println(f1.equals(f2));
+        System.out.println(portal.findFreelancersBySkill("Java"));
+        System.out.println(portal.sortJobsByPrice());
     }
 }
